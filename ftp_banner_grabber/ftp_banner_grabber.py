77dc1083aa2b0e26e65fb8ae11ebb0e314455e61
axi0m <kevin.dienst@kernelexploit.com>
2018-05-29 21:23:06 -0500

    Updated to PEP8 style

--- ftp_banner_grabber/ftp_banner_grabber.py
+++ ftp_banner_grabber/ftp_banner_grabber.py
@@ -4,26 +4,31 @@
 # changelog: 02/21/18 - initial creation
 #            02/23/18 - minor updates and rename file
 #            02/26/18 - adding vuln_banners.txt file to read in and iterate over, also changed name of script, had typo
+#            05/29/18 - adding blank lines per PEP8, renaming functions per PEP8
 
 import socket
 import sys
 import os
 
-def retBanner(ip, port):
+
+def ret_banner(ip, port):
     try:
         socket.setdefaulttimeout(2)
         s = socket.socket()
         s.connect((ip, port))
         banner = s.recv(1024)
         return str(banner)
+    # Fix exception handling here for specific cases
     except:
         return
 
-def checkVulns(banner):
+
+def check_vulns(banner):
     f = open("vuln_banners.txt", 'r')
     for line in f.readlines():
         if line.strip('\n') in banner:
             print("[+] Server is vulnerable: " + banner.strip('\n'))
+
 
 def main():
     if len(sys.argv) == 2:
@@ -40,10 +45,11 @@ def main():
             for x in range(1, 10):
                 ip = '192.168.1.' + str(x)
                 for port in portList:
-                    banner = retBanner(ip, port)
+                    banner = ret_banner(ip, port)
                     if banner:
-                        print('[+] ' + ip + ': ' + banner.strip('\n'))
-                        checkVulns(banner)
+                        print('[+] ' + ip + ': ' + banner.strip('\r\n'))
+                        check_vulns(banner)
+
 
 if __name__ == '__main__':
     main()
