9a4e1ace3d1d31abeaded5502f39a12ddcb3a4be
axi0m <kevin.dienst@kernelexploit.com>
2018-12-12 21:33:35 -0600

    Added threading and fixed functions a bit

--- port_scanner/portscanner.py
+++ port_scanner/portscanner.py
@@ -1,57 +1,78 @@
 # author: axi0m
-# purpose: rudimentary port scanner
+# purpose: rudimentary port scanner (TCP CONNECT port scan)
 # usage: portscanner.py --host --port
 # example: portscanner.py --host 10.1.1.1 --port 21,22,23
 # changelog: 12/11/18 - initial creation
+# 12/12/2018 - added threading, fixed functions up a bit, fixed byte format for socket data 
 '''
 
 To do:
+Add the following scan types
+TCP SYN, TCP XMAS, TCP FIN, TCP NULL, TCP SYN
+https://nmap.org/book/man-port-scanning-techniques.html
 
 '''
 
 import argparse
+import socket
+from threading import *
 from socket import *
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--host', nargs='?', action="store", dest='host', help="Host to scan.")
-parser.add_argument('--port', nargs='+', action="store", dest="port", help="Port(s) to scan, csv delimited")
-
-args = parser.parse_args()
-
-host = args.host
-port = args.port
-
-if (host == None) | (port == None):
-    parser.print_help()
-    exit(0)
+screenLock = Semaphore(value=1)
 
 def connScan(host, port):
     try: 
         connSocket = socket(AF_INET, SOCK_STREAM)
         connSocket.connect((host, port))
-        print("[+]{}/tcp open".format(port))
-        connSocket.close()
+        connSocket.send('SampleData\r\n'.encode('utf-8'))
+        results = connSocket.recv(100)
+        screenLock.acquire()
+        print("[+] {}/tcp open".format(port))
+        print("[+] {}".format(results.decode('utf-8')))
+    # We need to handle a lot more errors here!
+    
     except:
+        screenLock.acquire()
         print("[-] {}/tcp closed".format(port))
+
+    finally:
+        screenLock.release()
+        connSocket.close()
 
 def portScan(host, port):
     try:
         targetipv4 = gethostbyname(host)
     except:
+        
         print("[!] Cannot resolve {}".format(host))
         return
     try:
         targetname = gethostbyaddr(host)
-        print("\n[+] Scan Results for : {}".format(targetname[0]))
+        print("\nScan Results for: {}".format(targetname[0]))
     except:
-        print("\n[+] Scan Results for : {}".format(targetipv4))
+        print("\nScan Results for: {}".format(targetipv4))
 
-def main():
     setdefaulttimeout(5)
 
     for i in port:
-        print("Scanning port {}".format(i))
-        connScan(host, int(i))
+        #print("[!] Scanning port {}...".format(i)) # Not sure how we do this with threads?
+        t = Thread(target=connScan, args=(host, int(i)))
+        t.start()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', nargs='?', action="store", dest='host', help="Host to scan.")
+    parser.add_argument('--port', nargs='+', action="store", dest="port", help="Port(s) to scan, csv delimited")
+
+    args = parser.parse_args()
+
+    host = args.host
+    port = args.port
+
+    if (host == None) | (port == None):
+        parser.print_help()
+        exit(0)
+    portScan(host, port)
 
 if __name__ == '__main__':
     main()
