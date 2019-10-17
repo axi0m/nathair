a6d1c4329945d4d3da1b7bc44698a53e6b9967b3
Kevin Dienst <axi0m@users.noreply.github.com>
2019-10-17 15:31:40 -0500

    Add color output via colorama, resolve pylint suggestions

--- port_scanner/portscanner.py
+++ port_scanner/portscanner.py
@@ -4,22 +4,21 @@
 # example: portscanner.py --host 10.1.1.1 --port 21,22,23
 # changelog: 12/11/18 - initial creation
 # 12/12/2018 - added threading, fixed functions up a bit, fixed byte
-# format for socket data 
+# format for socket data
 
 '''
 
-To do:
-Add the following scan types
-TCP SYN, TCP XMAS, TCP FIN, TCP NULL, TCP SYN
-https://nmap.org/book/man-port-scanning-techniques.html
+TODO: Add the following scan types
+TODO: TCP SYN, TCP XMAS, TCP FIN, TCP NULL, TCP SYN
+TODO: https://nmap.org/book/man-port-scanning-techniques.html
 
-Add Unit Tests
-Add Logging
-Add Colorized Output via colorama
-Add pprint type functionality
-Add output via JSON
+TODO: Add Unit Tests
+TODO: Add Logging
+TODO: Add Colorized Output via colorama
+TODO: Add pprint type functionality
+TODO: Add output via JSON
 
-Weird bug in recv from port 22, results are returned and are bytes but for w/e 
+Weird bug in recv from port 22, results are returned and are bytes but for w/e
 reason the CLI hangs and never completes. Blocking mode issue?
 
 '''
@@ -27,70 +26,93 @@ reason the CLI hangs and never completes. Blocking mode issue?
 import argparse
 import socket
 import threading
-import sys
-import logging
+from colorama import Fore, init
 
-screenLock = threading.Semaphore(value=1)
+# init colorama
+init()
 
-def connScan(host, port):
+SCREEN_LOCK = threading.Semaphore(value=1)
+
+def conn_scan(host, port):
+    ''' TCP scan and banner receiver'''
     try:
         socket.setdefaulttimeout(1)
-        connSocket = socket.socket()
-        connSocket.connect((host, port))
-        connSocket.send('SampleData\r\n'.encode('utf-8'))
-        results = connSocket.recv(100)
-        screenLock.acquire()
-        print("[+] {}/tcp open".format(port))
-        print("[+] {}".format(results.decode('utf-8')))
+        conn_socket = socket.socket()
+        conn_socket.connect((host, port))
+        conn_socket.send('SampleData\r\n'.encode('utf-8'))
+        results = conn_socket.recv(100)
+        SCREEN_LOCK.acquire()
+        print(Fore.LIGHTYELLOW_EX + f"[+] {port}/tcp open")
+        print(Fore.GREEN + f"[+] {results.decode('utf-8')}")
 
-    except TimeoutError:
-        screenLock.acquire()
-        print("[!] Connection timeout on port {}".format(port))
+    except TimeoutError as timeout_error:
+        SCREEN_LOCK.acquire()
+        print(Fore.LIGHTYELLOW_EX + f"[!] Connection timeout on port {port}: {timeout_error}")
 
-    except Exception as e:
-        logging.error("Exception while trying to connect: {}".format(e))
-        screenLock.acquire()
-        print("[-] {}/tcp closed".format(port))
+    except KeyboardInterrupt as keybd_err:
+        SCREEN_LOCK.acquire()
+        print(Fore.LIGHTYELLOW_EX + f"[!] Keyboard interrupt: {keybd_err}")
+
+    except Exception as generic_err:
+        SCREEN_LOCK.acquire()
+        print(Fore.LIGHTYELLOW_EX + f"[-] Generic exception port most likely closed or network timeout: {generic_err}")
+        print(Fore.LIGHTYELLOW_EX + f"[-] {port}/tcp closed")
 
     finally:
-        screenLock.release()
-        connSocket.close()
+        SCREEN_LOCK.release()
+        conn_socket.close()
 
 
-def portScan(host, port):
+def port_scan(host, port):
+    ''' Perform scan for given hostname and TCP port number(s)'''
     try:
         targetipv4 = socket.gethostbyname(host)
-    except Exception as e:
-        print("[!] Cannot resolve {}".format(host))
-        return
-    try:
-        targetname = socket.gethostbyaddr(host)
-        print("\nScan Results for: {}".format(targetname[0]))
-    except Exception as e:
-        logging.error("Exception encountered during address resolution: {}".format(e))
-        print("\nScan Results for: {}".format(targetipv4))
+        print(Fore.LIGHTYELLOW_EX + f"[-] DEBUG Host IP address resolved via DNS: {targetipv4}")
 
-    socket.setdefaulttimeout(1)
+    except KeyboardInterrupt as keybd_err:
+        print(Fore.RED + f"[!] ERROR Keyboard interrupt handled: {keybd_err}")
+        return None
 
-    for i in port:
-        t = threading.Thread(target=connScan, args=(host, int(i)))
-        t.start()
+    except Exception as generic_err:
+        print(Fore.RED + f"[!] ERROR cannot resolve host {host}: {generic_err}")
+        return None
+
+    if targetipv4 is not None:
+        try:
+            targetname = socket.gethostbyaddr(host)
+            print(f"DNS results for: {targetname[0]}")
+        except Exception as generic_err:
+            print(f"[!] ERROR Exception encountered during address resolution: {generic_err}")
+            print(f"DNS results for: {targetipv4}")
+
+        socket.setdefaulttimeout(1)
+
+        for tcp_port in port:
+            thread_object = threading.Thread(target=conn_scan, args=(host, int(tcp_port)))
+            thread_object.start()
 
 
 def main():
+    ''' Main function call to parse arguments and run port scan'''
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', nargs='?', action="store", dest='host', help="Host to scan.")
-    parser.add_argument('--port', nargs='+', action="store", dest="port", help="Port(s) to scan, csv and space delimited")
+    parser.add_argument('--port', nargs='+', action="store", dest="port", \
+        help="Port(s) to scan, csv and space delimited")
 
     args = parser.parse_args()
 
     host = args.host
     port = args.port
 
-    if host is None or port is None:
+    if host is None:
         parser.print_help()
-        sys.exit(0)
-    portScan(host, port)
+        exit(0)
+
+    if port is None:
+        parser.print_help()
+        exit(0)
+
+    port_scan(host, port)
 
 
 if __name__ == '__main__':
