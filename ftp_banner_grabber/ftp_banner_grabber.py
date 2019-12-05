e22b879b48816b34ac90e5f03bee5e8a3e4b4883
Kevin Dienst <axi0m@users.noreply.github.com>
2019-12-05 17:32:21 -0600

    Implement argparse for vuln banner file parameter

--- ftp_banner_grabber/ftp_banner_grabber.py
+++ ftp_banner_grabber/ftp_banner_grabber.py
@@ -1,17 +1,10 @@
 # author: axi0m
 # purpose: banner grabber
 # usage: ftp_banner_grabber.py <vuln banner file>
-# changelog: 02/21/18 - initial creation
-#            02/23/18 - minor updates and rename file
-#            02/26/18 - adding vuln_banners.txt file to read in and iterate over, also changed name of script, had typo
-#            05/29/18 - adding blank lines per PEP8, renaming functions per PEP8
-#            10/17/19 - Per QL finding, adding handler for subclasses of BaseException - https://lgtm.com/rules/6780080/
-
 
 import socket
 import sys
 import os
-import logging
 import argparse
 
 def ret_banner(ip, port):
@@ -37,32 +30,45 @@ def ret_banner(ip, port):
 
 
 def check_vulns(banner):
-    with open("vuln_banners.txt", "r") as vuln_file:
+    with open(banner, "r") as vuln_file:
         for line in vuln_file.readlines():
             if line.strip('\n') in banner:
                 print(f"[+] Server is vulnerable! {banner}")
 
 
 def main():
-    if len(sys.argv) == 2:
-        filename = sys.argv[1]
-        if not os.path.isfile(filename):
-            print(f"[!] ERROR {filename} does not exist!")
-            exit(1)
-        if not os.access(filename, os.R_OK):
-            print(f"[!] ERROR {filename} Access Denied.")
-            exit(1)
-        else:
-            print(f"[+] Reading Vulnerabilities From: {filename}")
-            portList = [21]
-            for x in range(1, 10):
-                ip = '192.168.1.' + str(x)
-                for port in portList:
-                    banner = ret_banner(ip, port)
-                    if banner:
-                        stripped_banner = banner.strip('\r\n')
-                        print(f"[+] {ip}:{port} {stripped_banner}")
-                        check_vulns(stripped_banner)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file',nargs='?',action='store',dest='vulnfile',help="Filename with vulnerable banners to match against")
+    
+    args = parser.parse_args()
+
+    vulnerable_banner_file = args.vulnfile
+    
+    # Ensure we were provided a file as parameter
+    if not vulnerable_banner_file:
+        print(f'[!] ERROR vulnerable banner file not provided')
+        parser.print_help()
+
+    # Ensure file exists
+    if not os.path.isfile(vulnerable_banner_file):
+        print(f"[!] ERROR {vulnerable_banner_file} does not exist!")
+        exit(1)
+    
+    # Ensure we can read the file
+    if not os.access(vulnerable_banner_file, os.R_OK):
+        print(f"[!] ERROR {vulnerable_banner_file} Access Denied.")
+        exit(1)
+
+    print(f"[+] Reading vulnerabilities from: {vulnerable_banner_file}")
+    portList = [21]
+    for x in range(1, 10):
+        ip = '192.168.1.' + str(x)
+        for port in portList:
+            banner = ret_banner(ip, port)
+            if banner:
+                stripped_banner = banner.strip('\r\n')
+                print(f"[+] {ip}:{port} {stripped_banner}")
+                check_vulns(stripped_banner)
 
 
 if __name__ == '__main__':
