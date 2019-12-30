930b5466328c0d858d85eff9f5df70ea6aafc7e5
axi0m <axi0m@users.noreply.github.com>
2019-12-30 15:07:26 -0600

    Add colorama, fix few bugs, clean up output

--- ftp_banner_grabber/ftp_banner_grabber.py
+++ ftp_banner_grabber/ftp_banner_grabber.py
@@ -6,34 +6,38 @@ import socket
 import os
 import argparse
 import ipaddress
+from colorama import Fore, init
+
+# init colorama
+init()
 
 def ret_banner(ip, port):
     try:
         socket.setdefaulttimeout(2)
         s = socket.socket()
         s.connect((ip, port))
-        print(f"[+] Established network connection with host {ip} using port {port}")
+        print(Fore.YELLOW + f"[+] Established network connection with host {ip} using port {port}")
         banner = s.recv(1024)
         return f"{banner}"
 
     except SystemExit as sys_exit:
-        print(f"[!] ERROR operation failed, system exit: {sys_exit}")
+        print(Fore.LIGHTYELLOW_EX + f"[!] ERROR operation failed, system exit: {sys_exit}")
         return None
 
     except KeyboardInterrupt as keybd_err:
-        print(f"[!] ERROR operation canceled via Keyboard Interrupt: {keybd_err}")
+        print(Fore.LIGHTYELLOW_EX + f"[!] ERROR operation canceled via Keyboard Interrupt: {keybd_err}")
         return None
 
     except Exception as generic_err:
-        print(f"[!] ERROR unable to establish socket connection to IP address {ip} on TCP port {port}: {generic_err}")
+        print(Fore.LIGHTYELLOW_EX + f"[!] ERROR unable to establish socket connection to IP address {ip} on TCP port {port}: {generic_err}")
         return None
 
 
-def check_vulns(banner):
-    with open(banner, "r") as vuln_file:
-        for line in vuln_file.readlines():
-            if line.strip('\n') in banner:
-                print(f"[+] Server is vulnerable! {banner}")
+def check_vulns(reference_file, test_banner):
+    with open(reference_file, "r") as ref_file:
+        for line in ref_file.readlines():
+            if line.strip('\n') in test_banner:
+                print(Fore.GREEN + f"[+] Server is vulnerable! {test_banner}")
 
 
 def main():
@@ -47,40 +51,41 @@ def main():
     vulnerable_banner_file = args.vulnfile
     target_ipv4_range = args.iprange
 
+    # Ensure we have target IPv4 range
+    if not target_ipv4_range:
+        print(Fore.LIGHTYELLOW_EX + f'[!] ERROR - You must provide an IPv4 range in CIDR notation!')
+        parser.print_help()
+        exit(1)
+
     # Use IPAddress module to parse CIDR range and validate it
     network_hosts = ipaddress.ip_network(target_ipv4_range)
 
     # Use temporary variable for parsed ports parameter
     target_ports = args.ports
-    
-    # Ensure we have target IPv4 range
-    if not target_ipv4_range:
-        print(f'[!] ERROR - You must provide an IPv4 range in CIDR notation!')
+
+    # Ensure we were provided port(s) to check
+    if not target_ports:
+        print(Fore.LIGHTYELLOW_EX + f'[!] ERROR - Port list not provided')
         parser.print_help()
         exit(1)
 
     # Ensure we were provided a file as parameter
     if not vulnerable_banner_file:
-        print(f'[!] ERROR - Vulnerable banner file not provided')
-        parser.print_help()
-        exit(1)
-
-    # Ensure we were provided port(s) to check
-    if not target_ports:
+        print(Fore.LIGHTYELLOW_EX + f'[!] ERROR - Vulnerable banner file not provided')
         parser.print_help()
         exit(1)
 
     # Ensure file exists
     if not os.path.isfile(vulnerable_banner_file):
-        print(f"[!] ERROR - {vulnerable_banner_file} does not exist!")
+        print(Fore.LIGHTYELLOW_EX + f"[!] ERROR - {vulnerable_banner_file} does not exist!")
         exit(1)
     
     # Ensure we can read the file
     if not os.access(vulnerable_banner_file, os.R_OK):
-        print(f"[!] ERROR - {vulnerable_banner_file} Access Denied.")
+        print(Fore.LIGHTYELLOW_EX + f"[!] ERROR - {vulnerable_banner_file} Access Denied.")
         exit(1)
 
-    print(f"[+] INFO - Reading vulnerabilities from: {vulnerable_banner_file}")
+    print(Fore.YELLOW + f"[+] INFO - Reading vulnerabilities from: {vulnerable_banner_file}")
     
     # .hosts() method creates a generator for all our hosts in the network
     for ip in network_hosts.hosts():
@@ -89,11 +94,13 @@ def main():
             string_ip = str(ip)
             # Must convert port from str to integer for ret_banner function
             int_port = int(port)
+            # Get the banner from the remote host and store as string to compare
             banner = ret_banner(string_ip, int_port)
             if banner:
+                # Easier to strip it of erroneous characters before comparing
                 stripped_banner = banner.strip('\r\n')
-                print(f"[+] {ip}:{port} {stripped_banner}")
-                check_vulns(stripped_banner)
+                # DEBUG print(f"[+] {ip}:{port} {stripped_banner}")
+                check_vulns(vulnerable_banner_file, stripped_banner)
 
 
 if __name__ == '__main__':
